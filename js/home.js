23ee53f604751c65e843f38d8276df1d73fb0d85
wleygoncalves <39389579+wleygoncalves@users.noreply.github.com>
2018-05-17 20:08:17 -0300

    Delete home.js

--- js/home.js
+++
@@ -1,8 +0,0 @@
-function validaBusca(){
-	if (document.querySelector('#q').value == '') {
-		alert.show('Não podia ter deixado em branco a busca!');
-		return false;
-	}
-	document.querySelector('#form-busca').onsubmit = validaBusca;
-}
-
